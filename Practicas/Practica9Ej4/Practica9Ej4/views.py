22c6d717ad443d69fffd1fb5b8b6ac5ea7b9d94d
DavidBurgosDaw <daw202505@estudiantes.salesianasnsp.es>
2025-02-03 13:56:44 +0100

    Practica9Ej4 sin terminar (No sale el contenido)

---
+++ Practicas/Practica9Ej4/Practica9Ej4/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from Practica9Ej4.Models.Lenguaje import Plantilla
+def index(request):
+    x = [
+        Plantilla(nombre = "Java", año = 2000, descripcion = "Lenguaje de programacion orientado a objetos procedente de C"),
+        Plantilla(nombre = "C#", año = 1999, descripcion = "Lenguaje de programacion orientado a objetos procedente de C"),
+        Plantilla(nombre = "JavaScript", año = 2005, descripcion = "Lenguaje de programacion orientado a web"),
+        Plantilla(nombre = "Python", año = 2010, descripcion = "Lenguaje de programacion no orientado a objetos, muy bueno para inteligencias artificiales" )
+    ]
+
+          
+  
+    return render(request,"index.html",  {"plantillas" : x})
